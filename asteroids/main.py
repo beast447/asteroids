69f68e2305df97301ae344aececf1d4e0c69f38f
Trevor King <wildbeast447@gmail.com>
2025-11-29 15:54:09 -0500

    Added a frame rate limiter and variable to store delta time

--- asteroids/main.py
+++ asteroids/main.py
@@ -7,6 +7,8 @@ running = True
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
@@ -17,5 +19,6 @@ def main():
                 return
         screen.fill("black")
         pygame.display.flip()
+        dt = clock.tick(60) / 1000 
 if __name__ == "__main__":
     main()
